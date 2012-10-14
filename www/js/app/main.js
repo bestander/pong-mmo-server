0719ddd12269833bce5b490f905bcada804759d6
Konstantin Raev <bestander@gmail.com>
2012-10-14 15:48:37 +1300

    somewhat first working attempt of using Volo as js package manager. Can't say I ma very happy but it may do

--- www/js/app/main.js
+++ www/js/app/main.js
@@ -6,6 +6,28 @@ define(['jquery', 'facebook-api!appId:' + facebook_app_id], function ($, FB) {
       });
     });
     require(['jquery', 'jquery.facebook.multifriend.select'], function($, fbFriends){
-      $("#jfmfs-container").jfmfs();
+      FB.getLoginStatus(function(response) {
+        if (response.status === "connected") {
+          init();
+        } else {
+          // no user session available, someone you dont know
+        }
+      });
+
+      function login() {
+        FB.login(function(response) {
+          if (response.status === "connected") {
+            init();
+          } else {
+            alert('Login Failed!');
+          }
+        });
+      }
+
+      function init() {
+        FB.api('/me', function(response) {
+          $("#jfmfs-container").jfmfs({ max_selected: 15, max_selected_message: "{0} of {1} selected"});
+        });
+      }
     });
   });
