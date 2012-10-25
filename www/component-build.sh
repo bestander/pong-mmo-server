09d0b867cd9dc80f4c4884061ed6d5c811056975
Konstantin Raev <bestander@gmail.com>
2012-10-26 00:59:20 +1300

    support for component tool as front end package manager, this one like the most tolearable

---
+++ www/component-build.sh
@@ -0,0 +1,1 @@
+component build -o js/build
---
+++ www/js/index.js
@@ -0,0 +1,49 @@
+/*
+* Author: Konstantin Raev (bestander@gmail.com)
+* Released under the MIT license
+*/
+// expose jquery for plugins
+window.jQuery = window.$ = require("jquery");
+
+var $ = window.$
+  , facebookLoader = require("./lib/facebook/loader.js")
+  , friendsSelector = require("jquery-facebook-multi-friend-selector")
+  ;
+
+$(function(){
+  $("#login").click(
+	  function () {
+	    console.log("clicked button");
+	  });
+  // TODO friends selector initialize here
+});
+
+exports.initFacebook = function(facebookAppId){
+	facebookLoader.load(facebookAppId, function(){
+		FB.getLoginStatus(function (response) {
+		  if (response.status === "connected") {
+		    init();
+		  } else {
+		    console.log("no user session available");
+		    login();
+		  }
+		});
+	});
+}
+
+function login() {
+  FB.login(function (response) {
+    if (response.status === "connected") {
+      init();
+    } else {
+      alert('Login Failed!');
+    }
+  });
+}
+
+function init() {
+  FB.api('/me', function (response) {
+    console.log(response.name);
+    $("#jfmfs-container").jfmfs({ max_selected: 15, max_selected_message: "{0} of {1} selected"});
+  });
+}
