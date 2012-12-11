2a1ac1a4a7e7f8670c23a69ee607e41ce354269a
Konstantin Raev <bestander@gmail.com>
2012-12-11 16:42:49 +1300

    bdd

--- test/worldSocketSync.spec.js
+++ test/worldSocketSync.spec.js
@@ -1,6 +1,28 @@
 
-describe('Test', function () {
-  it('should pass', function () {
-    var a = 1;
+describe('Game sync module', function () {
+
+  describe('should start a new game when first connection is added', function () {
+
   });
+
+  describe('should allow a second socket to be added to an existing game ', function () {
+
+  });
+
+  describe('should notify game if one socket is closed and the other is still connected', function () {
+
+  });
+
+  describe('should stop the game if no sockets are connected', function () {
+
+  });
+
+  describe('should periodically notify connected sockets about world state', function () {
+
+  });
+
+  describe('should pass through sockets commands to game object as players commands', function () {
+
+  });
+
 });
